02b1e19e038ff28dd1735be66d729f7bd006f1b5
Felipe de Jesus <32593546+FJAConceicao@users.noreply.github.com>
2022-04-09 12:13:12 -0300

    atualiza token busca_repos.py

--- busca_repos.py
+++ busca_repos.py
@@ -12,7 +12,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Conecta ao github por meio de token
-token = "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"
+token = "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"
 github = Github(token)
 
 class Repository(db.Model):
